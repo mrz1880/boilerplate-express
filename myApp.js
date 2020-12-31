ae35012ff6105444060ad223d377506bcf85699e
mrz1880 <mrz1880@gmail.com>
2020-12-31 10:49:12 +0100

    test with absolutePath variable

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const app = express();
-
+const absolutePath = `${__dirname}/views/index.html`;
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/views/index.html`);
+  res.sendFile(absolutePath);
 });
 
 module.exports = app;
